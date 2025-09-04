9be178a9b91fbf8f3bafa5724d185333d8ae27eb
Daysies <days2cecili@gmail.com>
2025-09-04 18:26:54 -0400

    Completed Task 1: Changed width Teammate1 Dayra

--- Game.py
+++ Game.py
@@ -77,7 +77,7 @@ bg_color = pygame.Color('grey12')
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)  # Ball (centered)
 # TODO Task 1 Make the paddle bigger
 player_height = 15
-player_width = 100
+player_width = 200
 player = pygame.Rect(screen_width/2 - 45, screen_height - 20, player_width, player_height)  # Player paddle
 
 # Game Variables
