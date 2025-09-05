0a982d3bf7dd534672c08d0dbcdd8bd832a7acce
Yerikelite <yerik.fernandez@upr.edu>
2025-09-05 18:59:21 -0400

    Task #2 Complete: Fix scoring functionality.

--- Game.py
+++ Game.py
@@ -22,7 +22,7 @@ def ball_movement():
     if ball.colliderect(player):
         if abs(ball.bottom - player.top) < 10:  # Check if ball hits the top of the paddle
             # TODO Task 2: Fix score to increase by 1
-            score = 1  # Increase player score
+            score += 1  # Increase player score
             ball_speed_y *= -1  # Reverse ball's vertical direction
             # TODO Task 6: Add sound effects HERE
 
